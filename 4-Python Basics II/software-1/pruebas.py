cdc6bbd7b82b8230c8c7ef8e8bd6674df7f08776
Steven Bonilla <placidobc08@gmail.com>
2022-04-07 12:27:42 -0600

    commit

--- 4-Python Basics II/software-1/pruebas.py
+++ 4-Python Basics II/software-1/pruebas.py
@@ -1,3 +1,25 @@
 
+def broo():
+
+  NUMEROS = 0
 
 
+  while True:
+    try:
+        nombre = input("Escribe tu nombre \n")
+        a = int(nombre)
+    except ValueError:
+        print("Nombre valido")
+        break
+
+    if (a) +- NUMEROS:
+        print("El nombre no puede tener numeros.")
+        continue
+
+    if (a) == NUMEROS:
+        print("El nombre no puede tener numeros")
+        continue
+    
+   
+
+broo()
