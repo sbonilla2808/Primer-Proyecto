2fd9b706d77f259ef775d599b0e1d29cbaecf271
steve28P <placidobc08@gmail.com>
2022-05-14 00:08:22 -0600

    commit

---
+++ Clasificacion_Estudiantil.py
@@ -0,0 +1,56 @@
+
+class Utils():
+
+    @staticmethod
+    def ask_id():
+        id_correct = False
+        while not id_correct:
+            try:
+                user_id = int(input("Escribe el ID del estudiante: \n"))
+            except ValueError:
+                print("Solo se permiten numeros.")
+                continue
+
+            if user_id > 999999999:
+                print("Incorrecto, tienes numeros de mas.")
+                continue
+
+            if user_id < 100000000:
+                print("Incorrecto, te faltan numeros.")
+                continue
+            id_correct = True
+        return str(user_id)
+
+class Db():
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def find_id_in_db(self, cedula):
+        with open(self.filename, 'r') as dbfile:
+            row = 0
+            lines = dbfile.readlines()
+            for line in lines:
+                #print(line)
+                columns = line.split(',')
+                #print(columns)
+                if cedula==columns[0]:
+                    return row
+                row += 1
+            return False
+            row = -1
+            return row
+
+def insert_grade():
+    row = -1
+    db = Db('Excel_Bases.csv')
+    while row < 1:
+        cedula = Utils.ask_id()
+        row = db.find_id_in_db(cedula)
+        if db.find_id_in_db(cedula) is False:
+            print(f'The id {cedula} was not found')
+            break
+        print("Correct ID")
+        pass
+    
+insert_grade()
